fa1c3434769c815cc2ba4f70356a986ae19cdb31
Sunil Pokhrel <96107188+sunilpokhrel1@users.noreply.github.com>
2023-03-30 02:23:23 +0545

    Add files via upload
    ActionAndFuncDelegates

---
+++ ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionAndFuncDelegates
+{
+    class Program
+    {
+
+        // Define an Action delegate that takes two integers as input parameters
+        static Action<int, int> addNumbers = (x, y) => Console.WriteLine($"The sum of {x} and {y} is: {x + y}");
+
+        // Define a Func delegate that takes two integers as input parameters and returns their sum
+        static Func<int, int, int> sumDelegate = (a, b) => a + b;
+
+        static void Main(string[] args)
+        {
+            // Invoke the Action delegate with two integer values
+            addNumbers(10, 20);
+
+            // Call the method represented by the Func delegate and print the result
+            int sum = sumDelegate(5, 10);
+            Console.WriteLine($"The sum of 5 and 10 is: {sum}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
